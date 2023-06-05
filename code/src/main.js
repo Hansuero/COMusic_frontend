b178ee19fe4cbc2d5f8a3e4f3458da6f87a4c028
Younsur <111101727+Younsur@users.noreply.github.com>
2023-06-05 15:38:47 +0800

    修改接口

--- code/src/main.js
+++ code/src/main.js
@@ -15,6 +15,10 @@ var cur_user = {
 }
 app.provide('is_login', is_login)
 app.config.globalProperties.$axios = axios
+//服务器
+axios.defaults.baseURL = 'http://82.157.165.72/api'
+//本地
+//axios.defaults.baseURL ='http://127.0.0.1:4523/m1/2749792-0-default/api‘
 app.config.globalProperties.$cur_user = cur_user
 app.use(router)
 app.use(ElementPlus)
