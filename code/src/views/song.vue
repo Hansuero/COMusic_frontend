8be30bc484c9ed252009b91a8b5cadbadc2a20a4
Younsur <111101727+Younsur@users.noreply.github.com>
2023-06-06 17:44:39 +0800

    歌曲详情（播放器样式还得改）

--- code/src/views/song.vue
+++ code/src/views/song.vue
@@ -1,45 +1,60 @@
 <template>
-	<nav>
-		<NavNoLeft/>
-	</nav>
-	<div class="back">
-		<el-button type="text" id="backButton" @click="goBack">返回</el-button>
-	</div>
-	<div class="info">
-		<p id="songName">{{songInfo.songName}}</p>
-		<p id="singer">{{songInfo.singer}}</p>
-	</div>
-	<div class="body">
-		<img id="img" alt="img" src="{{ songInfo.song_cover }}">
-		<div id="lyric">
-			<textarea id="content" v-model="lyric.content" style="resize: none" cols="53" rows="80"></textarea>
-		</div>
-	</div>
-	<div class="play">
-		<div id="block">
-			<el-slider :max="100" :min="0" @change="valToTime(time)" v-model="time" :format-tooltip="valToTime(time)"></el-slider>
-		</div>
-		<div id="control">
-			<el-icon :size="35" id="minus"><minus /></el-icon>
-      <el-icon :size="45" id="pause" v-if="isPlay.judge" @click="button_pause"><audio loop="loop" src="../assets"></audio><video-pause /></el-icon>
-      <el-icon :size="45" id="continue" v-else @click="button_continue"><audio loop="loop" src="../assets"></audio><video-play /></el-icon>
+  <nav>
+    <NavNoLeft/>
+  </nav>
+  <div class="back">
+    <el-button type="text" id="backButton" @click="goBack">返回</el-button>
+  </div>
+  <div class="info">
+    <p id="songName">{{songInfo.songName}}</p>
+    <p id="singer">{{songInfo.singer}}</p>
+  </div>
+  <div class="body">
+    <img id="img" alt="img" src="{{ songInfo.song_cover }}">
+    <div id="lyric">
+      <textarea id="content" v-model="lyric.content" style="resize: none" cols="53" rows="80"></textarea>
+    </div>
+  </div>
+  <div class="play">
+    <!--<div id="block">
+      <el-slider :max="100" :min="0" @change="valToTime(time)" v-model="time" :format-tooltip="valToTime(time)"></el-slider>
+    </div>
+    <div id="control">
+      <el-icon :size="35" id="minus"><minus /></el-icon>
+      <el-icon :size="45" id="pause" v-if="isPlay.judge" @click="button_pause"><video-pause /></el-icon>
+      <el-icon :size="45" id="continue" v-else @click="button_continue"><video-play /></el-icon>
+      <audio ref="audio" loop="loop" src="{{ songInfo.song_url }}" style="display: none"></audio>
       <el-icon :size="35" id="plus"><plus /></el-icon>
-		</div>
-		<div id="buttons">
-			<el-button color="#7eec52" id="complain"><strong><el-icon size=23px id="icon4"><warning /></el-icon>{{ buttons.complain }}</strong></el-button>
-			<el-button color="#7eec52" id="collect"><strong><el-icon size=23px id="icon3"><star /></el-icon>{{ buttons.collect }}</strong></el-button>
-			<el-button @click="goToComment" color="#7eec52" id="comment"><strong><el-icon size=23px id="icon2"><chat-line-square /></el-icon>{{ buttons.comment }}</strong></el-button>
-			<el-button color="#7eec52" id="like"><strong><el-icon size=23px id="icon1"><lollipop /></el-icon>{{ buttons.like }}</strong></el-button>
-		</div>
-	</div>
+    </div>-->
+    <audio @play="button_continue" @pause="button_pause" style="position: relative;float: left;left: 15%;top: 12px" ref="audio" loop="loop" src="{{ songInfo.song_url }}" controls></audio>
+    <div id="buttons">
+      <el-button color="#7eec52" id="complain"><strong><el-icon size=23px id="icon4"><warning /></el-icon>{{ buttons.complain }}</strong></el-button>
+      <el-button v-if="!judge.collected" @click="collect" color="#7eec52" id="collect"><strong><el-icon size=23px id="icon3"><star /></el-icon>{{ buttons.collect }}</strong></el-button>
+      <el-button v-else @click="collect" color="#7eec52" id="collect"><strong style="color: lightcoral"><el-icon size=23px id="icon3"><star /></el-icon>{{ buttons.collect }}</strong></el-button>
+      <el-button @click="goToComment" color="#7eec52" id="comment"><strong><el-icon size=23px id="icon2"><chat-line-square /></el-icon>{{ buttons.comment }}</strong></el-button>
+      <el-button color="#7eec52" id="like"><strong><el-icon size=23px id="icon1"><lollipop /></el-icon>{{ buttons.like }}</strong></el-button>
+    </div>
+    <el-drawer title="请选择收藏夹" v-model="forDrawer.drawer" :direction="forDrawer.dir" :before-close="handleClose" destroy-on-close>
+      <div class="hide">
+        <ul type="none" id="favos">
+          <li v-for="(item, index) in favos.title" ref="favoLi" :key="index" :value="item" @click="clickFavo(index, nums.f_num[index])"> {{ item }} </li>
+        </ul>
+      </div>
+      <div>
+        <el-button @click="close" type="text" style="position: relative;left: 48.5%;font-size: larger">确认</el-button>
+      </div>
+    </el-drawer>
+  </div>
 </template>
 
 <script>
 import NavNoLeft from '@/components/NavNoLeft.vue'
 import router from '@/router'
+import { useRoute } from 'vue-router'
 import store from '@/store'
-import { reactive, onMounted} from 'vue'
+import { reactive, onMounted, ref } from 'vue'
 import axios from 'axios'
+import { ElDrawer } from 'element-plus'
 import { Minus } from '@element-plus/icons'
 import { Plus } from '@element-plus/icons'
 import { VideoPause } from '@element-plus/icons'
@@ -51,39 +66,82 @@ import { Lollipop } from '@element-plus/icons'
 
 
 export default {
-	name: 'song',
-	components: {
-		NavNoLeft,
-		Minus,
-		VideoPause,
-		VideoPlay,
-		Plus,
-		Star,
-		Warning,
-		ChatLineSquare,
-		Lollipop
-	},
-  props: {
-    'song_id': Number
+  name: 'song',
+  components: {
+    NavNoLeft,
+    Minus,
+    VideoPause,
+    VideoPlay,
+    Plus,
+    Star,
+    Warning,
+    ChatLineSquare,
+    Lollipop,
+    ElDrawer
   },
-	setup (props) {
-		const songInfo = reactive({
-			songName: '',
-			singer: '',
-      song_cover: ''
-		})
-    const lyric = reactive({
-      content: '你的回话凌乱着\n在这个时刻\n我想起喷泉旁的白鸽\n甜蜜散落了\n情绪莫名的拉扯\n' +
-        '我还爱你呢\n而你断断续续唱着歌\n假装没事了\n时间过了走了\n爱情面临选择\n你冷了倦了我哭了\n' +
-        '离开时的不快乐\n你用卡片手写着\n有些爱只给到这\n真的痛了\n怎么了你累了说好的\n幸福呢\n' +
-        '我懂了不说了\n爱淡了梦远了\n'
+
+  setup () {
+    const forDrawer = reactive({
+      drawer: false,
+      dir: 'btt'
     })
+    function handleClose () {
+      let close = confirm("确认关闭?")
+      if (close) {
+        forDrawer.drawer = false
+      }
+    }
+    function close () {
+      if (favo_id.fid !== -1) {
+        forDrawer.drawer = false
+        const form_data = new FormData()
+        form_data.append('songs_id', songInfo.song_id)
+        form_data.append('playlist_id', favo_id.fid)
+        axios.post('/music/add_song_to_favo', form_data,{
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then(
+          function (response) {
+            if (response.status === 200) {
+              favo_id.fid = -1
+              alert('收藏成功')
+              judge.collected = true
+            }
+          }
+        )
+      } else {
+        alert("请选择收藏夹")
+      }
+    }
+    const songInfo = reactive({
+      song_url: '',
+      song_id: useRoute().params.id,
+      songName: '',
+      singer: '',
+      song_cover: ''
+    })
+    const favo_id = reactive({
+      fid: -1
+    })
+    const lyric = reactive({
+      content: ''
+    })
+    const favos = reactive({
+      id: [],
+      title: []
+    })
+    const nums = reactive({
+      f_num: []
+    })
+    const favoLi = ref([])
     onMounted(()=>{
       axios.get('/music/get_song', {
-        params: {song_id: props.song_id}
+        params: {song_id: songInfo.song_id}
       }).then(
         function (response) {
           if (response.status === 200) {
+            songInfo.song_url = response.data.song_url
             songInfo.songName = response.data.song_name
             songInfo.singer = response.data.singer
             lyric.content = response.data.lyric
@@ -91,34 +149,86 @@ export default {
           }
         }
       )
+      axios.get('/music/get_favo_list').then(
+        function (response) {
+          if (response.status === 200) {
+            favos.id = []
+            favos.title = []
+            for (var i = 0; i < response.data.favo_list.length; i++) {
+              favos.id.push(response.data.favo_list[i].favo_id)
+              favos.title.push(response.data.favo_list[i].favo_title)
+              nums.f_num[i] = 1
+            }
+          }
+        }
+      )
+      favoLi.value.forEach(item=>{
+        item.style.background = "#00bfff"
+      })
     })
-		const time = reactive({
-			timeValue: 0
-		})
-		const isPlay = reactive({
-			judge: store.state.play
-		})
-		const buttons = reactive({
-			like: '我喜欢',
-			comment: '评论',
-			collect: '收藏',
-			complain: '投诉'
-		})
-		function goBack () {
-			router.back()
-		}
-		function goToComment () {
-			router.push('./comment')
-		}
-		function valToTime (val) {
-			return val
-		}
-		function button_pause () {
-			isPlay.judge = false
-		}
-		function button_continue () {
-      var form_data = FormData()
-      form_data.append('song_id', props.song_id)
+    const time = reactive({
+      timeValue: 0
+    })
+    const isPlay = reactive({
+      judge: store.state.play
+    })
+    const buttons = reactive({
+      like: '我喜欢',
+      comment: '评论',
+      collect: '收藏',
+      complain: '投诉'
+    })
+    function goBack () {
+      router.back()
+    }
+    const judge = reactive({
+      collected: false
+    })
+    function collect () {
+      if (!judge.collected){
+        forDrawer.drawer = true
+      } else {
+        var cancel = confirm("是否取消收藏?")
+        if (cancel) {
+          judge.collected = false
+        }
+      }
+    }
+    function clickFavo (index, num) {
+      if (num === 1) {
+        favoLi.value.forEach(item=>{
+          if (favoLi.value.indexOf(item) === index) {
+            item.style.background = "#eeec22"
+            favo_id.fid = favos.id[index]
+            nums.f_num[index] = 2
+          } else {
+            item.style.background = "#00bfff"
+            nums.f_num[favoLi.value.indexOf(item)] = 1
+          }
+        })
+      }
+      else if (num === 2) {
+        favoLi.value.forEach(item=>{
+          if (favoLi.value.indexOf(item) === index) {
+            item.style.background = "#00bfff"
+            favo_id.fid = -1
+            nums.f_num[index] = 1
+          }
+        })
+      }
+    }
+    function goToComment () {
+      router.push('../comment/' + songInfo.song_id)
+    }
+    function valToTime (val) {
+      return val
+    }
+    function button_pause () {
+      isPlay.judge = false
+    }
+    function button_continue () {
+      var form_data = new FormData()
+      form_data.append('song_id', songInfo.song_id)
       axios.post('/music/add_to_recent', form_data, {
         headers: {
           'Content-Type': 'multipart/form-data'
@@ -130,203 +240,227 @@ export default {
           }
         }
       )
-		}
-		return {
-			goBack,
-			goToComment,
-			songInfo,
-			time,
-			valToTime,
-			isPlay,
-			button_pause,
-			button_continue,
-			buttons,
-			lyric
-		}
-	}
+    }
+    return {
+      forDrawer,
+      handleClose,
+      close,
+      goBack,
+      favos,
+      nums,
+      favoLi,
+      judge,
+      collect,
+      clickFavo,
+      goToComment,
+      songInfo,
+      time,
+      valToTime,
+      isPlay,
+      button_pause,
+      button_continue,
+      buttons,
+      lyric
+    }
+  }
 }
 </script>
 
 <style scoped>
 *{
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
 }
 nav {
-	margin: 0;
-	padding: 0;
-	background-attachment: fixed;
+  margin: 0;
+  padding: 0;
+  background-attachment: fixed;
 }
 nav a {
-	font-weight: bold;
-	color: white;
-	text-decoration: none;
-	font-size: 20px;
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
 }
 .back{
-	height: 2%;
+  height: 2%;
 }
 .back #backButton{
-	font-size: 18px;
-	text-decoration: underline;
-	color: black;
-	position: relative;
-	left: 2%;
+  font-size: 18px;
+  text-decoration: underline;
+  color: black;
+  position: relative;
+  left: 2%;
 }
 .info{
-	height: 60px;
-	text-align: center;
-	font-family: 'Microsoft YaHei', sans-serif;
+  height: 60px;
+  text-align: center;
+  font-family: 'Microsoft YaHei', sans-serif;
 }
 .info #songName{
-	line-height: 25px;
-	font-size: 25px;
-	margin-bottom: 5px;
+  line-height: 25px;
+  font-size: 25px;
+  margin-bottom: 5px;
 }
 .info #singer{
-	line-height: 16px;
-	font-size: 16px;
+  line-height: 16px;
+  font-size: 16px;
 }
 .body{
-	height: 450px;
-	font-family: 'Microsoft YaHei', sans-serif;
+  height: 450px;
+  font-family: 'Microsoft YaHei', sans-serif;
 }
 .body #img{
-	height: 99.5%;
-	width: 31.2%;
-	border: 1px solid #7eec52;
-	position: relative;
-	left: 15%;
-	float: left;
+  height: 99.5%;
+  width: 31.2%;
+  border: 1px solid #7eec52;
+  position: relative;
+  left: 15%;
+  float: left;
 }
 .body #lyric{
-	height: 100%;
-	width: 31.25%;
-	position: relative;
-	right: 15%;
-	float: right;
-	overflow-y: auto;
+  height: 100%;
+  width: 31.25%;
+  position: relative;
+  right: 15%;
+  float: right;
+  overflow-y: auto;
 }
 .body #lyric #content{
-	font-size: 16px;
-	border: none;
-	color: #2c3e50;
-	text-align: center;
-	line-height: 35px;
-	overflow: hidden;
+  font-size: 16px;
+  border: none;
+  color: #2c3e50;
+  text-align: center;
+  line-height: 35px;
+  overflow: hidden;
 }
 .play{
-	height: 60px;;
-	padding: 20px 10px 20px;
+  height: 60px;;
+  padding: 20px 10px 20px;
 }
 .play #block{
-	width: 31.2%;
-	position: relative;
-	top: 25%;
-	left: 10%;
-	float: left;
+  width: 31.2%;
+  position: relative;
+  top: 25%;
+  left: 10%;
+  float: left;
 }
 .play #control{
-	width: 13.9%;
-	height: 100%;
-	position: relative;
-	left: 10%;
-	text-align: left;
-	float: left;
+  width: 13.9%;
+  height: 100%;
+  position: relative;
+  left: 10%;
+  text-align: left;
+  float: left;
 }
 .play #control audio{
   width: 100%;
   height: 100%;
 }
 .play #control #minus{
-	position: relative;
-	left: 12%;
-	top: 5%;
+  position: relative;
+  left: 12%;
+  top: 5%;
 }
 .play #control #pause{
-	position: relative;
-	left: 18%;
-	top: 12%;
+  position: relative;
+  left: 18%;
+  top: 12%;
 }
 .play #control #continue{
-	position: relative;
-	left: 18%;
-	top: 12%;
+  position: relative;
+  left: 18%;
+  top: 12%;
 }
 .play #control #plus{
-	position: relative;
-	left: 24%;
-	top: 5%
+  position: relative;
+  left: 24%;
+  top: 5%
 }
 .play #buttons{
-	width: 46%;
-	height: 100%;
-	position: relative;
-	left: 8.7%;
-	float: left;
+  width: 46%;
+  height: 100%;
+  position: relative;
+  left: 27%;
+  float: left;
 }
 .play #buttons #like{
-	margin: 0;
-	float: right;
-	position: relative;
-	right: 34.8%;
-	top: 21%;
-	width: 15.4%;
-	height: 58.3%;
-	line-height: 35px;
-	color: white;
-	font-size: 18px;
+  margin: 0;
+  float: right;
+  position: relative;
+  right: 34.8%;
+  top: 21%;
+  width: 15.4%;
+  height: 58.3%;
+  line-height: 35px;
+  color: white;
+  font-size: 18px;
 }
 .play #buttons #icon1{
-	position: relative;
-	top: 5px;
+  position: relative;
+  top: 5px;
 }
 .play #buttons #comment{
-	margin: 0;
-	float: right;
-	position: relative;
-	right: 27.8%;
-	top: 21%;
-	width: 15.4%;
-	height: 58.3%;
-	line-height: 35px;
-	color: white;
-	font-size: 18px;
+  margin: 0;
+  float: right;
+  position: relative;
+  right: 27.8%;
+  top: 21%;
+  width: 15.4%;
+  height: 58.3%;
+  line-height: 35px;
+  color: white;
+  font-size: 18px;
 }
 .play #buttons #icon2{
-	position: relative;
-	top: 5px;
+  position: relative;
+  top: 5px;
 }
 .play #buttons #collect{
-	margin: 0;
-	float: right;
-	position: relative;
-	right: 20.1%;
-	top: 21%;
-	width: 15.4%;
-	height: 58.3%;
-	line-height: 35px;
-	color: white;
-	font-size: 18px;
+  margin: 0;
+  float: right;
+  position: relative;
+  right: 20.1%;
+  top: 21%;
+  width: 15.4%;
+  height: 58.3%;
+  line-height: 35px;
+  color: white;
+  font-size: 18px;
 }
 .play #buttons #icon3{
-	position: relative;
-	top: 5px;
+  position: relative;
+  top: 5px;
 }
 .play #buttons #complain{
-	margin: 0;
-	float: right;
-	position: relative;
-	right: 12.3%;
-	top: 21%;
-	width: 15.4%;
-	height: 58.3%;
-	line-height: 35px;
-	color: white;
-	font-size: 18px;
+  margin: 0;
+  float: right;
+  position: relative;
+  right: 12.3%;
+  top: 21%;
+  width: 15.4%;
+  height: 58.3%;
+  line-height: 35px;
+  color: white;
+  font-size: 18px;
 }
 .play #buttons #icon4{
-	position: relative;
-	top: 5px;
+  position: relative;
+  top: 5px;
+}
+.hide{
+  overflow: auto;
+}
+.hide #favos li{
+  position: relative;
+  left: 5%;
+  width: 90%;
+  height: 42px;
+  line-height: 42px;
+  text-align: center;
+  color: white;
+  background: #00bfff;
+  border-radius: 10px;
+  margin-bottom: 20px;
 }
 </style>
